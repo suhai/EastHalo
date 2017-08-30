2d234927f13ed01fda7a7cd3be8db2c2b528b136
suhai yehuza <soyehuza@gmail.com>
2017-08-30 10:44:46 -0700

    initial commit; fresh off the rails with setups

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Root from './root';
+
+const App = () => (
+  <div>
+		<Root />
+	</div>     
+);
+
+export default App;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Root = () => (
+  <div>
+    <h1>Welcome to EastHalo</h1>
+  </div>
+);
+
+export default Root;

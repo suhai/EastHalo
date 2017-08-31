d047d96ca4d6a9e62d9b66893fa94bf91c148c5d
suhai yehuza <soyehuza@gmail.com>
2017-08-31 11:06:09 -0700

    set up student - professor relationship

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,2 +1,13 @@
 class Professor < User
+
+	def students
+		studs = []
+		self.courses.each do |course|
+			course.students.each do |student|
+				studs << student
+			end
+		end
+		
+		studs.uniq
+	end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,12 @@
 class Student < User
 	has_many :posts
+
+	def professors
+		profs = []
+		self.courses.each do |course|
+			profs << course.professor
+		end
+		
+		profs.uniq
+	end
 end

144405a7579a1fa32b833f9f85d9f196de9cd745
suhai yehuza <soyehuza@gmail.com>
2017-08-31 02:24:46 -0700

    fixed user courses bug

--- app/models/course_enrollment.rb
+++ app/models/course_enrollment.rb
@@ -3,20 +3,26 @@ class CourseEnrollment < ApplicationRecord
 	belongs_to :student, class_name: "User", :foreign_key => "user_id"
 	
 
+	after_create :create_reciprocal, unless: :already_created?
+  after_destroy :remove_reciprocal, if: :already_created?
 
-	
-		# after_create :add_self_to_professor_courses, unless: :already_in_it?
-		# after_destroy :remove_self_to_professor_courses, if: :already_in_it?
-	
-		# def add_self_to_professor_courses
-		# 	Professor.find(self.professor_id).courses << self
+	def create_reciprocal
+		if self.class == 'Course'
+			Student.find(self.students.last.student_id).courses << self
+		elsif self.class == 'Student'
+			Course.find(self.courses.last.course_id).students << self
+		end
+	end
+
+	def remove_reciprocal
+		# if self.class == 'Course'
+		# 	Student.find(self.students.last.student_id).courses.delete(self)
+		# elsif self.class == 'Student'
+		# 	Course.find(self.courses.last.course_id).students.delete(self)
 		# end
-	
-		# def remove_self_to_professor_courses
-		# 	Professor.find(self.professor_id).courses.delete(self)
-		# end
-	
-		# def already_in_it?
-		# 	Professor.find(self.professor_id).courses.include?(self)
-		# end
+	end
+
+	def already_created?
+		# Professor.find(self.professor_id).courses.include?(self)
+	end
 end

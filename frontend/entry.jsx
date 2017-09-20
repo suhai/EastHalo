4407edbe7700e8502310dedc976d9764df299e39
suhai yehuza <soyehuza@gmail.com>
2017-09-20 11:44:33 -0700

    updated readme

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import App from './components/App';
+import App from "./components/App";
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;

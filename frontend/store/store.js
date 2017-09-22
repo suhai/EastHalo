f1e85ff87d446ef37ceeac214ff793600e5094b1
suhai yehuza <soyehuza@gmail.com>
2017-09-21 17:23:05 -0700

    commented out redux-logger for production deployment purpose

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,14 +2,14 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 
 // const createLogger = require(redux-logger);
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== 'production') {
-  middlewares.unshift(createLogger());
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   middlewares.unshift(createLogger());
+// }
 
 const configureStore = (preloadedState = {}) => (
   createStore(
